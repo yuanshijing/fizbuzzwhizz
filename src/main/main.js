5b778e743bbf88590ddaa5f530894bee8f4f5904
yuanshijing <826774822@qq.com>
2017-08-09 09:58:26 +0000

    finish

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-function fizbuzzwhizz(a, b, c){
-  // write your code here...
+function fizbuzzwhizz(a, b, c, i) {
+    var result;
+    var num = i.split();
+    for (let j in num) {
+        if (num[j] === a) {
+            result = 'Fizz';
+        }
+    }
+else
+    if (i % a === 0 && i % b === 0 && i % c === 0) {
+        result = 'FizzBuzzWhizz';
+    } else if (i % a === 0 && i % b === 0) {
+        result = 'FizzBuzz';
+    } else if (i % b === 0 && i % c === 0) {
+        result = 'BuzzWhizz';
+    } else if (i % c === 0 && i % a === 0) {
+        result = 'FizzWhizz';
+    } else if (i % a === 0) {
+        result = 'Fizz';
+    } else if (i % b === 0) {
+        result = 'Buzz';
+    } else if (i % c === 0) {
+        result = 'Whizz';
+    }
+    else {
+        result = i;
+    }
+    return result;
 }
+// write your code here...
--- src/test/main-test.js
+++ src/test/main-test.js
@@ -3,7 +3,103 @@
 describe('fizbuzzwhizz', () => {
 
   // write your tests here...
-  it('should do something when some condition', () => {
-    expect(false).toEqual(true);
+it("should console 'Fizz' when 'special a is b the same time'", () => {
+       //GIVEN
+    let a = 3;
+    let b = 5;
+    let c = 7;
+    let i = 30;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+     //THEN
+    expect(result).toEqual('Fizz');
+  })
+it("should console 'Fizz' when 'normal a times'", () => {
+    //GIVEN
+    let a = 3;
+    let b = 5;
+    let c = 7;
+    let i = 6;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+    //THEN
+    expect(result).toEqual('Fizz');
   });
-});
+  it("should console 'Buzz' when 'b times'", () => {
+      //GIVEN
+    let a = 3;
+    let b = 5;
+    let c = 7;
+    let i = 10;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+     //THEN
+    expect(result).toEqual('Buzz');
+  });
+   it("should console 'Whizz'when 'c times'", () => {
+       //GIVEN
+    let a = 3;
+    let b = 5;
+    let c = 7;
+    let i = 14;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+     //THEN
+    expect(result).toEqual('Whizz');
+  })
+     it("should console 'FizzBuzz' when 'a and b the same times'", () => {
+       //GIVEN
+    let a = 3;
+    let b = 5;
+    let c = 7;
+    let i = 15;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+     //THEN
+    expect(result).toEqual('FizzBuzz');
+  })
+   it("should console 'BuzzWhizz' when 'b and c the same times'", () => {
+       //GIVEN
+    let a = 3;
+    let b = 5;
+    let c = 7;
+    let i = 70;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+     //THEN
+    expect(result).toEqual('BuzzWhizz');
+  })
+     it("should console 'FizzWhizz' when 'a and c the same times'", () => {
+       //GIVEN
+    let a = 3;
+    let b = 5;
+    let c = 7;
+    let i = 21;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+     //THEN
+    expect(result).toEqual('FizzWhizz');
+  })
+   it("should console 'FizzBuzzWhizz' when 'a , b , c the same times'", () => {
+       //GIVEN
+    let a = 2;
+    let b = 3;
+    let c = 5;
+    let i = 60;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+     //THEN
+    expect(result).toEqual('FizzBuzzWhizz');
+  })
+  it("should console i when 'else'", () => {
+       //GIVEN
+    let a = 3;
+    let b = 5;
+    let c = 7;
+    let i = 2;
+    //WHEN
+    let result = fizBuzzwhizz(a,b,c,i);
+     //THEN
+    expect(result).toEqual(i);
+  })
+
